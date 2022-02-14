fc59f9228b9645c40b8b37017e4001c4e6b9a35b
Prathamesh <hundareprathamesh0009@gmail.com>
2022-02-14 14:07:26 +0530

    My commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import About from "./components/about/About"
+import Contact from "./components/contact/Contact"
+import Intro from "./components/intro/Intro"
+
+const App = () => {
+    return (
+        <div>
+            <Intro/>
+            <About/>
+            <Contact/> 
+        </div>
+    )
+}
+
+export default App
